ee8fe0ee9896e6255d39f873319f90e944302db4
Chris Whong <chris.whong@socrata.com>
2014-11-04 19:55:00 -0500

    In class geojson demo

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.pn
 
 var marker = L.marker([40.68025, -74.00155]).addTo(map);
 marker.bindPopup("Larry's house").openPopup();
+
+function popUp(feature, layer) {
+  layer.bindPopup(feature.properties.name);
+}
+
+
+//Get external geoJSON file
+var geojsonLayer = new L.GeoJSON.AJAX("./js/testgeojson.json",{onEachFeature:popUp});
+
+geojsonLayer.addTo(map);
